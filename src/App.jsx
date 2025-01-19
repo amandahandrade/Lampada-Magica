68ffe0584a10765fec470c4b55ee03b744b11a9b
amandahandrade <amandah.andrades@gmail.com>
2025-01-19 15:06:05 -0300

    Reformulando a função mudarImagem para que possa alternar entre as duas imagens|

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import lampadaMagica from "./assets/lampadaMagica.png";
 import genioLampada from "./assets/genioLampada.png";
-import "./app.scss"
+import "./app.scss";
 
 import { useState } from "react";
 
@@ -11,19 +11,35 @@ export default function App() {
   const [imagem, setImagem] = useState(lampadaMagica);
 
   // função para a imagem ser trocada com arrow function
-  const mudatImagem = () => {
-    setImagem(genioLampada);
+  const mudarImagem = () => {
+    // O imagemAtual é o valor atual da variável imagem
+    setImagem((estadoAtual) =>
+      // Operador Condicional Ternário
+    estadoAtual === lampadaMagica ? genioLampada : lampadaMagica
+    );
   };
+
+  // Outra forma de realizar a função co if e else:
+  // const mudarImagem = () => {
+  //   setImagem((imagemAtual) => {
+  //     if (imagemAtual === lampadaMagica) {
+  //       return genioLampada;
+  //     } else {
+  //       return lampadaMagica; 
+  //     }
+  //   });
+  // };
+
   return (
     <main>
-        <p>"Liberte a magia da programação com a lâmpada de Aladim."</p>
+      <p>"Liberte a magia da programação com a lâmpada de Aladim."</p>
       <img
         src={imagem}
         alt="Imagem da lâmpada mágica / Imagem do gênio da lâmpada do Aladdin"
       />
 
       {/* onClick: um evento que escuta quando o usuário clica no botão */}
-      <button onClick={mudatImagem}>Clique Aqui</button>
+      <button onClick={mudarImagem}>Clique Aqui</button>
     </main>
   );
 }
